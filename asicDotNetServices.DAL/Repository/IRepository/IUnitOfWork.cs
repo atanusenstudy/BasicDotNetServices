6f64f199b8d64dbf4f2b8a66fe666aa6e815027f
atanusenstudy <43321179+atanusenstudy@users.noreply.github.com>
2023-02-19 21:45:02 +0530

    Implemented Repository Pattern and UnitOfWork (#5)

---
+++ asicDotNetServices.DAL/Repository/IRepository/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicDotNetServices.DAL.Repository.IRepository
+{
+    /// <summary>
+    /// This will do all the database Operations rather than individual repository
+    /// </summary>
+    public interface IUnitOfWork
+    {
+        IContactRepository Contact { get; }
+        void Save();
+    }
+}
